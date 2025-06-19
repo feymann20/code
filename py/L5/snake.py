1899d015c5d92375c1df538f7ceac1a53c855825
Natã Hora <natahora2@gmail.com>
2025-06-19 14:21:15 -0300

    up

--- py/L5/snake.py
+++ py/L5/snake.py
@@ -1,18 +1,20 @@
-n,m=input().split()
-n=int(n)
-m=int(m)
+n,m=map(int,input().split())
 a=0
+b=0
 snake=[]
-for i in range(n):
-    x=[int(x) for x in input().split()]
-    snake.append(x)
-for i in range(n):
+for i in range(0,n,1):
+    cobrita=[int(x) for x in input().split()]
+    snake.append(cobrita)
+for i in range(0,n,1):
     if i%2==0:
-        for j in range(m):
+        for j in range(0,m,1):
             a=a+snake[i][j]
-        
+            print(snake[i][j])
     if i%2!=0:
         for j in range(m-1,-1,-1):
-            a=a+snake[i][j]
-
-print(a)
+            b=b+snake[i][j]
+            print(snake[i][j])
+if a+b>0:
+    print(a+b)
+else:
+    print(0)
