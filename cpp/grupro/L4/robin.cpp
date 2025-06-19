43a4d4b92aaffcdfed39493327f424fab9d60a91
Natã Hora <natahora2@gmail.com>
2025-06-19 19:29:23 -0300

    up

---
+++ cpp/grupro/L4/robin.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+    int i,E,n,m,aux;
+    queue<int> processo;
+    cin >> n >> m;
+    for(i=0;i<n;i++){
+        cin>>E;
+        processo.push(E);
+    }
+    for (i = 0; i < m; i++)
+    {
+    aux=processo.front()-1;
+    processo.pop();
+        if (aux>0)
+        {
+            processo.push(aux);
+        }
+    }
+    if(processo.size()!=0){
+        cout<<processo.size()<<endl;
+        while (!processo.empty())
+        {
+            cout << processo.front();
+            processo.pop();
+            cout<< " ";
+        }
+     cout<<endl;   
+    }
+
+    else{
+        cout<<"pronto"<<endl;
+    }
+}
