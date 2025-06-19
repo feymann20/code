69d330fee7762243c26f0169eb3116114d802e1e
Natã Hora <natahora2@gmail.com>
2025-06-19 19:29:54 -0300

    up

--- py/L5/snake.py
+++ py/L5/snake.py
@@ -1,20 +1,18 @@
 n,m=map(int,input().split())
 a=0
-b=0
 snake=[]
-for i in range(0,n,1):
+for _ in range(n): # underline é uma maneira de definir uma variavel morta
     cobrita=[int(x) for x in input().split()]
     snake.append(cobrita)
-for i in range(0,n,1):
+for i in range(n):
     if i%2==0:
-        for j in range(0,m,1):
+        for j in range(m):
             a=a+snake[i][j]
-            print(snake[i][j])
-    if i%2!=0:
+            if a<0:
+                a=0
+    else:
         for j in range(m-1,-1,-1):
-            b=b+snake[i][j]
-            print(snake[i][j])
-if a+b>0:
-    print(a+b)
-else:
-    print(0)
+            a=a+snake[i][j]
+            if a<0:
+                a=0
+print(a)
