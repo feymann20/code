b4ae7017c1d5401f6b77e0433d1ceee9cefbd89f
Natã Hora <natahora2@gmail.com>
2025-06-27 12:34:05 -0300

    up

--- cpp/grupro/L4/Tetrês.cpp
+++ cpp/grupro/L4/Tetrês.cpp
@@ -1,35 +1,51 @@
 #include<iostream>
 #include<stack>
+#include <queue>
 #include<string>
 using namespace std;
 
 int main(){
-    int n,m,i,num1,num2,pontos,total=0;
-    int bloco;
-    stack<int> q;
+    int n,m,i;
+    string num1,num2,a,string1;
+    int p=0,b=0;
+    string bloco;
+    stack<string> q,f;
     cin>>n>>m;
     for(i=0;i<n;i++){
         cin>>bloco;
         q.push(bloco);
-        }
-    while (!q.empty())
+    }
+    for (i = 0; i < n; i++)
     {
+        a="";
         num1=q.top();
+        string1=num1;
         q.pop();
-        num2=q.top();
-        total=num1+num2;
-        if (total%111==0)
-        {
-            pontos+=10;
-        }
-        if (total>m*100)
-        {
-            cout<<"game over"<<endl;
-        }
-        
-    
-        
+            if (q.size()!=0)
+            {
+                num2=q.top();
+                a=num1+num2;
+                if (a=="100011"||a=="011100" || a=="001110"||a=="110001" || a=="010101"|| a=="101010")
+                {
+                    p=p+10;
+                    q.pop();
+                }
+                else{
+                    f.push(string1);
+                    cout<< f.top() <<endl;
+                    if(f.size()>=m){
+                        cout<< "game over"<<endl;
+                        b=1;
+                        break;
+                    }
+                }
+
+            }
     }
     
+    
+    if(b==0){
+        cout << p << endl;
+    }
 
 }
--- cpp/grupro/L4/formiga.cpp
+++ cpp/grupro/L4/formiga.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <queue>
 using namespace std; // queue -> std
+struct coord
+{
+   int x;
+   int y;
+};
+
 int main() {
 	int n,i,j,num;
-   queue<int> q;
+   queue<coord> q;
    vector<int>tauto;
    cin>>n;
-   vector<vector<int>> terreno;
+   vector<vector<int>> terreno(n, vecotr<int>(n));
    for ( i = 0; i < n; i++){
       for ( j = 0; i < n; j++){
          cin >> num;
@@ -15,9 +21,14 @@ int main() {
    
    }
       int a,b;
+      coord aux;
       cin>>a>>b;
+      aux.x=a;
+      aux.y=b;
 
+      q.push(aux)w
         while (q.front()==1){
+         q.pop()
             if (terreno[i+1][j]==1 && i+1<n){
 
                q.push(terreno[i][j]);
