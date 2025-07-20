c6a76f07d76dcd1c5233702b38281ff0417e3497
Natã Hora <natahora2@gmail.com>
2025-07-20 09:01:53 -0300

    up

--- cpp/grupro/L4/formiga.cpp
+++ cpp/grupro/L4/formiga.cpp
@@ -1,50 +1,72 @@
 #include <iostream>
-#include <queue>
-using namespace std; // queue -> std
+#include <stack>
+#include <vector>
+using namespace std; 
 struct coord
 {
    int x;
    int y;
 };
 
-int main() {
-	int n,i,j,num;
-   queue<coord> q;
-   vector<int>tauto;
-   cin>>n;
-   vector<vector<int>> terreno(n, vecotr<int>(n));
-   for ( i = 0; i < n; i++){
-      for ( j = 0; i < n; j++){
-         cin >> num;
-         terreno[i][j]=num;
+int main()
+{
+   int n, i, j;
+   stack<coord> q;
+   cin >> n;
+   int terreno[n][n];
+   for (i = 0; i < n; i++)
+   {
+      for (j = 0; j < n; j++)
+      {
+         cin >> terreno[i][j];
       }
-   
    }
-      int a,b;
-      coord aux;
-      cin>>a>>b;
-      aux.x=a;
-      aux.y=b;
+   int a, b, area = 0;
+   coord aux,coordenada;
+   cin >> a >> b;
+   aux.x = a;
+   aux.y = b;
+   q.push(aux);
+   while (true)
+   {
+      if (terreno[a][b]==1){
+         area++;
+         terreno[a][b] = 0;
+      }
+      if (terreno[a + 1][b] == 1 && a + 1 < n)
+      {
+         aux.x=a+1;
+         aux.y=b;
+         q.push(aux);
+      }
 
-      q.push(aux)w
-        while (q.front()==1){
-         q.pop()
-            if (terreno[i+1][j]==1 && i+1<n){
+      if (terreno[a][b + 1] == 1 && b + 1 < n)
+      {
+         aux.x=a;
+         aux.y=b+1;
+         q.push(aux);
+      }
+      if (terreno[a - 1][b] == 1 && a > 0)
+      {
+         aux.x=a-1;
+         aux.y=b;
+         q.push(aux);
+      }
+      if (terreno[a][b - 1] == 1 && b> 0)
+      {
+         aux.x=a;
+         aux.y=b-1;
+         q.push(aux);
+      }
+      
+      if (q.empty()){
+         break;
+      }
+      coordenada=q.top();
+      a=coordenada.x;
+      b=coordenada.y;
+      q.pop();
 
-               q.push(terreno[i][j]);
-            }
-            if (terreno[i][j+1]==1 && j+1<n){
-
-               q.push(terreno[i][j]);
-            }
-            if (terreno[i-1][j]==1 && i-1>0){
-
-               q.push(terreno[i][j]);
-            }
-            if (terreno[i][j-1]==1 && j-1>0){
-
-               q.push(terreno[i][j]);
-         }
-        }
+   }
+   cout<<area<<endl;
 }
-
